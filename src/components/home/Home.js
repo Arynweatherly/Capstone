210d2a99e001e77f5a1408420478c2c2a11c0a98
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-10-26 17:44:41 -0500

    made a lot of changes to database

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import FriendsList from '../friends/FriendsList'
+import NotebookList from '../notebooks/NotebookList'
+import DeadlineList from '../deadlines/DeadlineList'
+import TaskList from '../tasks/TaskList'
+import { Card } from 'reactstrap'
+import './home.css'
 
 class Home extends Component {
   render() {
     return (
-      <address>
-        Easiest way to share classnotes online
-      </address>
+   <div>
+     <h2> Pass a Note Profile Page</h2>
+     <br />
+     <small>some sort of slogan</small>
+     {
+       <>
+       <Card className="friendsListCard">
+         <FriendsList 
+         currentUser={this.props.currentUser}
+         {...this.props}
+         />
+       </Card>
+       <Card>
+         <NotebookList 
+         currentUser={this.props.currentUser}
+         {...this.props}
+         />
+       </Card>
+       <Card>
+         <DeadlineList 
+         currentUser={this.props.currentUser}
+         {...this.props}
+         />
+       </Card>
+       <Card>
+         <TaskList 
+         currentUser={this.props.currentUser}
+         {...this.props}
+         />
+       </Card>
+       </>
+     }
+   </div>
     )
   }
 }
