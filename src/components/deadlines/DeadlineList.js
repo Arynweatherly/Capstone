be9769ed8d9eddf656edf1291ef4ce58880e12f8
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-11-24 15:57:12 -0500

    made some deletions

--- src/components/deadlines/DeadlineList.js
+++ src/components/deadlines/DeadlineList.js
@@ -1,61 +1,59 @@
-import React, { Component } from 'react'
-//import the components we will need
-import DeadlineCard from './DeadlineCard'
-import DeadlineManager from '../../modules/DeadlineManager'
-import './Deadline.css'
+import React, { Component } from "react";
+import DeadlineCard from "./DeadlineCard";
+import DeadlineManager from "../../modules/DeadlineManager";
+import "./Deadline.css";
 
 class DeadlineList extends Component {
-    //define what this component needs to render
-    state = {
-        deadlines: [],
-    }
+  //define what this component needs to render
+  state = {
+    deadlines: []
+  };
 
-componentDidMount(){
-  
-    //getAll from AnimalManager and hang on to that data; put it in state
-    DeadlineManager.getMyDeadlines(this.props.activeUser)
-    .then((deadlines) => {
-        this.setState({
-            deadlines: deadlines
-        })
-    })
-}
-deleteDeadline = id => {
-    DeadlineManager.delete(id)
-    .then(() => {
-      DeadlineManager.getMyDeadlines(parseInt(sessionStorage.getItem("activeUser")))
-      .then((newDeadlines) => {
-        this.setState({
-            deadlines: newDeadlines
-        })
-      })
-    })
+  componentDidMount() {
+    DeadlineManager.getMyDeadlines(this.props.activeUser).then(deadlines => {
+      this.setState({
+        deadlines: deadlines
+      });
+    });
   }
+  deleteDeadline = id => {
+    DeadlineManager.delete(id).then(() => {
+      DeadlineManager.getMyDeadlines(
+        parseInt(sessionStorage.getItem("activeUser"))
+      ).then(newDeadlines => {
+        this.setState({
+          deadlines: newDeadlines
+        });
+      });
+    });
+  };
 
-  render(){
-  
-
-    return(
-        <>
-      <div className="container-card-deadline">
-        {this.state.deadlines.map(deadline =>
-          <DeadlineCard
-            key={deadline.id}
-            deadline={deadline}
-            deleteDeadline={this.deleteDeadline}
-            {...this.props}
-          />
-        )}
-      </div>
-      <section className="section-content">
-            <button type="button"
-                className="btn"
-                onClick={() => {this.props.history.push("/deadlines/new")}}>
-    Add Date
-  </button>
-</section>
+  render() {
+    return (
+      <>
+        <div className="container-card-deadline">
+          {this.state.deadlines.map(deadline => (
+            <DeadlineCard
+              key={deadline.id}
+              deadline={deadline}
+              deleteDeadline={this.deleteDeadline}
+              {...this.props}
+            />
+          ))}
+        </div>
+        <section className="section-content">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              this.props.history.push("/deadlines/new");
+            }}
+          >
+            Add Date
+          </button>
+        </section>
       </>
-    )
+    );
   }
 }
 
