bb5b6b4378452a5acb97a4a13d01ca7c04db2f70
aryn weatherly <Arynweatherly93@gmail.com>
2020-01-01 16:26:37 -0600

    did stuff

--- src/components/notebooks/NotebookCard.js
+++ src/components/notebooks/NotebookCard.js
@@ -1,42 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./Notebook.css"
+import "./Notebook.css";
 
 class NotebookCard extends Component {
-
-  
   render() {
-      console.log(this.props)
+    console.log(this.props);
     return (
-
       <div className="tile is-ancestor card-container">
-<div className= "tile is-vertical is-parent notebook">
-  <div className="tile is-child box notebook">
-  <h3> <span className="card-notebookSubject">{this.props.notebook.subject}</span></h3>
-  < hr/>
-  <footer className="buttonsNotebookContainer">
-  <button type="button" className="buttonNotebook" onClick={() => this.props.deleteNotebook(this.props.notebook.id)}>Delete</button>
-  <button type="button" className="buttonNotebook" onClick={() => {this.props.history.push(`/notebooks/${this.props.notebook.id}/edit`)}}>Edit</button>
-  <button type="button" className="buttonNotebook" onClick={() => this.props.history.push(`/notebooks/${this.props.notebook.id}`)}>View Notes </button>
-  </footer>
-  {/* <footer class="card-footer is-centered"> */}
-{/* 
-    <div class="field is-grouped">
-  <p class="control">
-  <button type="button is-left" className="deleteNotebook" onClick={() => this.props.deleteNotebook(this.props.notebook.id)}>Delete</button>
-  <button type="button is-centered" className="editNotebooks" onClick={() => {this.props.history.push(`/notebooks/${this.props.notebook.id}/edit`)}}>Edit</button>
-  <button type="button is-right" className="viewNotebooks" onClick={() => this.props.history.push(`/notebooks/${this.props.notebook.id}`)}>View Notes </button> */}
-    {/* <Link to={`/notebooks/${this.props.notebook.id}`}>View Notes</Link> */}
-    {/* </p>
-    </div> */}
-  
-  
-  {/* </footer> */}
-</div>
-</div>
-</div>
+        <div className="tile is-vertical is-parent notebook">
+          <div className="tile is-child box notebook">
+            <h3>
+              {" "}
+              <span className="card-notebookSubject">
+                {this.props.notebook.subject}
+              </span>
+            </h3>
+            <hr />
+            <footer className="buttonsNotebookContainer">
+              <button
+                type="button"
+                className="buttonNotebook"
+                onClick={() =>
+                  this.props.deleteNotebook(this.props.notebook.id)
+                }
+              >
+                Delete
+              </button>
+              <button
+                type="button"
+                className="buttonNotebook"
+                onClick={() => {
+                  this.props.history.push(
+                    `/notebooks/${this.props.notebook.id}/edit`
+                  );
+                }}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="buttonNotebook"
+                onClick={() =>
+                  this.props.history.push(
+                    `/notebooks/${this.props.notebook.id}`
+                  )
+                }
+              >
+                View Notes{" "}
+              </button>
+            </footer>
+            {/* <footer class="card-footer is-centered"> */}
 
-    )
+            {/* </footer> */}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
