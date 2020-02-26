1a4bbb96b619364835b5f3fe81170a6f3dd05ea6
aryn weatherly <Arynweatherly93@gmail.com>
2020-02-26 14:46:46 -0500

    deleted stuff

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link, withRouter, Redirect } from "react-router-dom";
-import './Navbar.css'
-import FriendsList from '../friends/FriendsList'
+import "./Navbar.css";
+import FriendsList from "../friends/FriendsList";
 
 class NavBar extends Component {
   handleLogout = () => {
@@ -9,21 +9,23 @@ class NavBar extends Component {
     this.props.history.push("/");
   };
 
-  render(){
-
+  render() {
     return (
-      <header className ="navbarHead">
-
-
-        <h1 className="title is-3">Pass A Note<br />
+      <header className="navbarHead">
+        <h1 className="title is-3">
+          Pass A Note
+          <br />
           <small>The easiest way to share notes online</small>
         </h1>
         <nav>
           <ul className="container">
-          <img class="ProfilePic" src={`images/aryn.jpg`}/> 
-            <li><Link className="nav-link" to="/">Home</Link></li>
-            {/* <li><Link className="nav-link" to="/notebooks">Notebooks</Link></li> */}
-            {/* <li><Link className="nav-link" to="/login">Login</Link></li> */}
+            <img class="ProfilePic" src={`images/aryn.jpg`} />
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            {/* <li><Link className="nav-link" to="/notebooks">Notebooks</Link></li> */}{" "}
             {/* <li><Link className="nav-link" to="/tasks">Tasks</Link></li>
             <li><Link className="nav-link" to="/deadlines">Dates</Link></li> */}
             {this.props.user ? (
@@ -35,15 +37,16 @@ class NavBar extends Component {
             ) : (
               <Redirect to="/login" />
             )}
-            <li className='nav-item'>
-							<button size='small' onClick={this.handleLogout}>Logout</button>
-						</li>
+            <li className="nav-item">
+              <button size="small" onClick={this.handleLogout}>
+                Logout
+              </button>
+            </li>
           </ul>
         </nav>
-     
       </header>
-    )
+    );
   }
 }
 
-export default withRouter (NavBar);
+export default withRouter(NavBar);
